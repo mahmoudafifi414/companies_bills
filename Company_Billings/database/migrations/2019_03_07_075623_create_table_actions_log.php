8ae2676e50d29b0413d1d95014683ef9c9b4af88
mahmoudafifi414 <mahmoudafifi460@gmail.com>
2019-03-07 10:11:44 +0200

    add actions_log migration file to log the actions done in tables such delete action

--- Company_Billings/database/migrations/2019_03_07_075623_create_table_actions_log.php
+++ Company_Billings/database/migrations/2019_03_07_075623_create_table_actions_log.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateTableActionsLog extends Migration
 {
@@ -15,6 +15,10 @@ class CreateTableActionsLog extends Migration
     {
         Schema::create('actions_log', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('table_name');
+            $table->string('action');
+            $table->string('details')->nullable();
+            $table->tinyInteger('user');
             $table->timestamps();
         });
     }
